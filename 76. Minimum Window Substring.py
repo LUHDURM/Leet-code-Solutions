871e984f9aca7df8a9b0f6307e9441c31f32380c
MRUDHUL NR <156283594+LUHDURM@users.noreply.github.com>
2025-10-08 12:36:08 +0530

    Update 76. Minimum Window Substring.py

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -2,29 +2,24 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "":
             return ""
-
-        countT, window = {}, {}
+        count , window = {},{}
         for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
+            count[c] = 1 + count.get(c,0)
+        have , need = 0 , len(count)
+        res , reslen = [-1,-1] , float("infinity")
         l = 0
         for r in range(len(s)):
             c = s[r]
-            window[c] = 1 + window.get(c, 0)
-
-            if c in countT and window[c] == countT[c]:
+            window[c] = 1 + window.get(c,0)
+            if c in count and count[c] == window[c]:
                 have += 1
-
-            while have == need:
-                if (r - l + 1) < resLen:
-                    res = [l, r]
-                    resLen = r - l + 1
-
+            while need == have:
+                if r-l+1 < reslen:
+                    res = [l,r]
+                    reslen = r-l+1
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if s[l] in count and window[s[l]] < count[s[l]]:
                     have -= 1
                 l += 1
-        l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
+        l,r = res
+        return s[l:r+1] if reslen != float("infinity") else ""
