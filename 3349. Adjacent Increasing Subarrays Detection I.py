4768b896ba5f28f12f2f4e2202741e365e138881
MRUDHUL NR <156283594+LUHDURM@users.noreply.github.com>
2025-10-14 23:40:45 +0530

    Update 3349. Adjacent Increasing Subarrays Detection I.py

--- 3349. Adjacent Increasing Subarrays Detection I.py
+++ 3349. Adjacent Increasing Subarrays Detection I.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        best = 0
+        previous = 0
+        current = 1
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i-1]:
+                current += 1
+            else:
+                best = max(best , current//2 , min(previous,current))
+                previous = current
+                current = 1
+        best = max(best , current//2 , min(current,previous))
+        return best >= k
+        
