8fc6dca261a8c8550115e0cb62b1726b39c062e9
MRUDHUL NR <156283594+LUHDURM@users.noreply.github.com>
2025-11-20 20:18:20 +0530

    Update 17. Letter Combinations of a Phone Number.py

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -16,23 +16,22 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-
-        phone_map = {
-            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        combinations = {
+            "2":"abc" , "3":"def" , "4":"ghi" , "5":"jkl" , "6":"mno" , "7":"pqrs" , "8":"tuv" , "9":"wxyz"
         }
         res = []
-
-        def backtrack(i, path):
+        def backtrack(i,path):
             if i == len(digits):
                 res.append("".join(path))
                 return
-            letters = phone_map[digits[i]]
+            letters = combinations[digits[i]]
             for ch in letters:
                 path.append(ch)
-                backtrack(i + 1, path)
-                path.pop() 
+                backtrack(i+1,path)
+                path.pop()
         backtrack(0,[])
         return res
+
+       
 
         
